83a43a9b2dcca905238df34dcedf4a2ae4d74724
Philipp Lypniakov <filipprayden@gmail.com>
2017-08-02 22:28:26 +0300

    Move to Redux:
    1. comment out all this.state content & this.State() methods

--- src/UserTable.js
+++ src/UserTable.js
@@ -7,11 +7,11 @@ import SearchUserBar from './SearchUserBar';
 class UserTable extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+/*     this.state = {
       name: '',
       search: '',
       users: []
-    }
+    } */
 
     this.handleInputName = this.handleInputName.bind(this);
     this.handleSearchName = this.handleSearchName.bind(this);
@@ -20,24 +20,24 @@ class UserTable extends Component {
   }
 
 handleInputName(name) {
-  this.setState({name: name});
+  // this.setState({name: name});
 }
 
 handleSearchName(search) {
-  this.setState({search: search});
+  // this.setState({search: search});
 }
 
 handleAddUserName(user) {
    const newUsers = [...this.state.users];
    newUsers.push(user);
-    this.setState({users: newUsers, name: ''})
+    // this.setState({users: newUsers, name: ''})
 }
 
 handleRemoveUser(id) {
    const newUsers = [...this.state.users];
    const index = newUsers.findIndex(user => { return user.id === id});
    newUsers.splice(index, 1);
-   this.setState({users: newUsers})
+  //  this.setState({users: newUsers})
 }
 
   
